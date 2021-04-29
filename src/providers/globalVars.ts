13950f942eef116ab00ecafaab1efa91e71b8303
Peko Roganovic <peko.roganovic@gmail.com>
2021-04-29 12:55:09 +0200

    gvars

--- src/providers/globalVars.ts
+++ src/providers/globalVars.ts
@@ -3,8 +3,9 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class GlobalVars {
   // baseURL = 'http://localhost:8080';
-  baseURL = 'http://79.101.21.143:8070/gis-vrbas-2.0.5';
-  baseURL1 = 'http://192.168.69.21:8070/gis-vrbas-2.0.5'; // lokalna adresa
+  // baseURL1 = 'http://localhost:8080';
+  baseURL = 'http://79.101.21.143:8070/gis-vrbas-2.1.1';
+  baseURL1 = 'http://192.168.69.21:8070/gis-vrbas-2.1.1'; // lokalna adresa
 
   constructor() {
   }
