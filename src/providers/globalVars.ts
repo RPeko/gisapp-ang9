e8c29e940d872958a580d5530928c6360f8694c8
Peko Roganovic <peko.roganovic@gmail.com>
2021-05-24 17:01:29 +0200

    proba

--- src/providers/globalVars.ts
+++ src/providers/globalVars.ts
@@ -4,10 +4,10 @@ import { Injectable } from '@angular/core';
 export class GlobalVars {
   // baseURL = 'http://localhost:8080';
   // baseURL1 = 'http://localhost:8080';
-  baseURL = 'https://vgis21.herokuapp.com';
-  baseURL1 = 'https://vgis21.herokuapp.com';
-  // baseURL = 'http://79.101.21.143:8070/gis-vrbas-2.2.0';
-  // baseURL1 = 'http://192.168.69.21:8070/gis-vrbas-2.2.0'; // lokalna adresa
+  // baseURL = 'https://vgis21.herokuapp.com';
+  // baseURL1 = 'https://vgis21.herokuapp.com';
+  baseURL = 'http://79.101.21.143:8070/gis-vrbas-2.2.0';
+  baseURL1 = 'http://192.168.69.21:8070/gis-vrbas-2.2.0'; // lokalna adresa
 
   constructor() {
   }
