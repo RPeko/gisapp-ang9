96ff13dd8577e405133f5f7b1f931d101d006def
Peko Roganovic <peko.roganovic@gmail.com>
2021-08-12 10:49:52 +0200

    mts coud

--- src/providers/globalVars.ts
+++ src/providers/globalVars.ts
@@ -6,8 +6,11 @@ export class GlobalVars {
   // baseURL1 = 'http://localhost:8080';
   // baseURL = 'https://vgis21.herokuapp.com';
   // baseURL1 = 'https://vgis21.herokuapp.com';
-  baseURL = 'http://79.101.21.143:8070/gis-vrbas-2.1.2';
-  baseURL1 = 'http://192.168.69.21:8070/gis-vrbas-2.1.2'; // lokalna adresa
+  // baseURL = 'http://79.101.21.143:8070/gis-vrbas-2.1.2';
+  baseURL = 'http://93.87.76.62:8080/gis-vrbas-2.1.2';
+  // baseURL1 = 'http://192.168.69.21:8070/gis-vrbas-2.1.2'; // lokalna adresa
+  baseURL1 = 'http://93.87.76.62:8080/gis-vrbas-2.1.2';
+
 
   constructor() {
   }
